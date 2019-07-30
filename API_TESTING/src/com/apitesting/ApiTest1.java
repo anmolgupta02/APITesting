7cbcfa55d41da78deb024c3a934e193b0b8e9b81
appventurez <appventurez@appventurez-HP-ProBook-440-G1>
2019-07-30 11:36:21 +0530

    Added Time response test and post test setup

--- API_TESTING/src/com/apitesting/ApiTest1.java
+++ API_TESTING/src/com/apitesting/ApiTest1.java
@@ -6,21 +6,30 @@ import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import utils.ConfigReader;
 
 public class ApiTest1 {
-
+	
+	ConfigReader cr;
+	
 	@Test
-	public void test() {
+	public void test() throws Exception {
 		
-		RestAssured.baseURI = "https://reqres.in/";
+		cr = new ConfigReader();
+		
+		RestAssured.baseURI = cr.readBaseUrl();
 		
 		RequestSpecification Request = RestAssured.given();
 		
-		Response response = Request.request(Method.GET,"/api/users/2");
+		Response response = Request.request(Method.GET, cr.readApi());
 		
 		String responsebody = response.body().asString();
 		
 		System.out.println("Response Body => "+ responsebody);
+		
+		Long time = response.getTime();
+		
+		System.out.println("TIme taken => " +time+" ms");
 		
 	}
 
---
+++ API_TESTING/src/com/apitesting/PostTest.java
@@ -0,0 +1,9 @@
+package com.apitesting;
+
+import org.json.simple.JSONObject;
+
+public class PostTest {
+
+	
+	
+}
